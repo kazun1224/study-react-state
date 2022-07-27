fa50c2c3288907c6914937cd4ce380a3807c23ad
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-07-27 12:07:00 +0900

    stateを試すための共通のアプリを作成

--- sample/src/pages/_document.tsx
+++ sample/src/pages/_document.tsx
@@ -4,7 +4,10 @@ export default function Document() {
   return (
     <Html lang='ja'>
       <Head>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kimeiga/bahunya/dist/bahunya.min.css"/>
+      <link
+          rel="stylesheet"
+          href="https://cdn.rawgit.com/kimeiga/bahunya/css/bahunya-0.1.2.css"
+        />
       </Head>
       <body>
         <Main />
